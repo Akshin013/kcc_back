31673773794f1bfa46bada5c41a52edff1b069aa
Akshin013 <akshingadimov@gmail.com>
2025-09-29 17:23:51 +0400

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB подключена"))
 .catch((err) => console.error("❌ Ошибка подключения:", err));
 
