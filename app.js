3ecdbb1dab09f18898f1cc830f47c6ff29fbb54b
Akshin013 <akshingadimov@gmail.com>
2025-09-29 17:51:27 +0400

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/api/favorites", favoritesRouter); 
-app.use("/api/cars", carsRouter);
+app.use("https://kcc-back.onrender.com/api/cars", carsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => 
